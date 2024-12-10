e04321b2b585a0e34f39020ba061993d2fc6b0ca
nhmnazmul22 <najmulmonir12345@gmail.com>
2024-12-10 12:42:01 +0600

    new cors domin set

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,7 @@ app.use(express.urlencoded({ max: MAX_JSON_FILE, extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://coding-solutions.netlify.app"],
     credentials: true,
   })
 );
